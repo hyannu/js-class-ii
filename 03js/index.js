e1672a024112054589aca7faaff747496b16798f
hyannu <annurao9560@gmail.com>
2026-06-07 13:32:31 +0530

    const keyword

--- 03js/index.js
+++ 03js/index.js
@@ -1,9 +1,48 @@
-//variable ko kaise bnate hai
-let name ="ARchana Rao";
-let age=23;
+//variable
+// let name ="annukumari";
+// let age=34;
+// console.log(name,age);
+//
 
+
+// let name ="annukumari";
+// let age=34;
+// age=23;
+// age=22;
+// console.log(name,age);
+
+
+// const use
+
+// let name ="annukumari";
+// let age=34;
+// age=23;
+// age=22;
+// age=21;
+// console.log(name,age);
+// const account=1234567890;
+// console.log(account);
+
+// let name ="annukumari";
+// let age=34;
+// age=23;
+// age=22;
+// age=21;
+// name="Archana rao";
+// console.log(name,age);
+// const account=1234567890;
+//  account=123;
+// console.log(account);
+
+var a=10;
+a=20;
+console.log(a);
+
+let name="anu kumari";
+let age =20;
+
+if(true){
+    let c = 90;
+}
 age=30;
-age=40;
-console.log(name,age);
-const accound=123456789;
-console.log(accound);
+console.log(c)
