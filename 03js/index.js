1d3716e93b3dcc5c36d5376d9ffd37e50e972846
hyannu <annurao9560@gmail.com>
2026-06-07 12:11:21 +0530

    initialize variable with value

--- 03js/index.js
+++ 03js/index.js
@@ -1,1 +1,9 @@
-console.log("Archana rao");
+//variable ko kaise bnate hai
+let name ="ARchana Rao";
+let age=23;
+
+age=30;
+age=40;
+console.log(name,age);
+const accound=123456789;
+console.log(accound);
