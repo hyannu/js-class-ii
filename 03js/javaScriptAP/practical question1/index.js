38004d959c55c98df3aaa1e2cf62e93660732ef8
hyannu <annurao9560@gmail.com>
2026-06-12 19:14:37 +0530

    variable name

--- 03js/javaScriptAP/practical question1/index.js
+++ 03js/javaScriptAP/practical question1/index.js
@@ -2,20 +2,32 @@
 //      <!-- Create a const object called "product" to store information shown in the picture. -->
 
 
-const product ={
-    title:"Ball Pen",
-    rating: 4,
-    offer: 5,
-    price: 270,
-};
+// const product ={
+//     title:"Ball Pen",
+//     rating: 4,
+//     offer: 5,
+//     price: 270,
+// };
 
-console.log(product);
-console.log(product["title"]);
-console.log(product["rating"]);
-console.log(product["price"]);
-console.log(typeof product);
+// console.log(product);
+// console.log(product["title"]);
+// console.log(product["rating"]);
+// console.log(product["price"]);
+// console.log(typeof product);
 
 // QUESTION NUMBER 2
 
 //    Create a const object called"prifile" to store information shown in the Picture.
 
+const profile={
+    userName:"@rlarchu",
+    followers:596,
+    folloing: 4,
+    post:195,
+    enterpreneur:"@archanarao",
+    threadeName:"@archana",
+    isfollow:true,
+    bio: "computer science engg.",
+};
+console.log(profile);
+console.log(typeof profile);
