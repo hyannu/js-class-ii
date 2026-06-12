95253e1b20094c3b92aa69a2daf62abfb799c979
hyannu <annurao9560@gmail.com>
2026-06-12 18:48:42 +0530

    answer

---
+++ 03js/javaScriptAP/practical question1/index.js
@@ -0,0 +1,16 @@
+//  <!-- QUESTION 1 -->
+//      <!-- Create a const object called "product" to store information shown in the picture. -->
+
+
+const product ={
+    title:"Ball Pen",
+    rating: 4,
+    offer: 5,
+    price: 270,
+};
+
+console.log(product);
+console.log(product["title"]);
+console.log(product["rating"]);
+console.log(product["price"]);
+console.log(typeof product);
