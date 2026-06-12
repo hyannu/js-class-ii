6175b3447fa8c0dab73e044c4b3421ee3178e091
hyannu <annurao9560@gmail.com>
2026-06-12 18:21:14 +0530

    practice question

--- 03js/javaScriptAP/index.js
+++ 03js/javaScriptAP/index.js
@@ -35,6 +35,37 @@
 // x=null;
 // console.log(isfollow,x);
 // bigint data type
-let x = BigInt("123");
-let y =Symbol("Hello!")
-console.log(x,typeof y);
+// let x = BigInt("123");
+// let y =Symbol("Hello!")
+// console.log(x,y,typeof y);
+
+// OBJECT DATA TYPE
+ const student={
+    fullName: "Archana Rao",
+    age:34,
+    cgpa:8.7,
+    ispass: true
+ };
+ student["age"] = student["age"] + 1;
+ console.log(student,typeof student);
+ console.log(student["age"]);
+ console.log(student["cgpa"]);
+ console.log(student["ispass"]);
+
+
+
+//  const employ={
+//    fullName:"Annu kumari",
+//      age: 45,
+//     salary: 45000,
+//     ot:"5 hour",
+//     month:"full",
+//     isPass: true
+//  }
+// console.log(employ);
+// console.log(typeof employ);
+// console.log(student["age"]);
+// console.log(student["fullName"]);
+//     //   ya
+// console.log(employ["fullname"]);//undefined
+// console.log(employ["age"]);
---
+++ 03js/javaScriptAP/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+const student={
+//     fullName: "Archana Rao",
+//     age:34,
+//     cgpa:8.7,
+//     ispass: true
+//  }
+//  console.log(student,typeof student);
