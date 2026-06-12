f0fe909ff060ada184b17775caaac3b0ae2c2188
hyannu <annurao9560@gmail.com>
2026-06-12 12:22:33 +0530

    Reserved key word in javascript

--- 03js/javaScriptAP/index.js
+++ 03js/javaScriptAP/index.js
@@ -1,3 +1,14 @@
-age=24;
-name="annu";
-console.log(age+1,name);
+// age=24;
+// name="annu";
+// price=99.99;
+// radius=14;
+// a= null;
+// y=undefined;
+// isfollow=false;
+// console.log(age+1,name,a,y,isfollow,price);
+
+// VARIABLE RULE
+ 
+console.log(Name_t);
+
+
