29cdfd417da19588859fad26f6376935a27b7a2a
hyannu <annurao9560@gmail.com>
2026-06-13 15:07:25 +0530

    Assignment Operator

--- 03js/javaScriptAP/javascript2/index.js
+++ 03js/javaScriptAP/javascript2/index.js
@@ -15,7 +15,10 @@
 
 
 
-
-// let a = 5;
-// let b = 2;
-// console.log("a = ",a, "& b= ", b);
+// Unary operator
+let a = 5;
+let b = 2;
+console.log("a = ",a, "& b= ", b);
+// console.log("a++ = ", a++);  
+// console.log("a-- = ", a--);  
+console.log("--a = ", --a);  
