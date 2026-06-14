be949bf59b1f22bcf194c38b98ef1793901071f8
hyannu <annurao9560@gmail.com>
2026-06-14 16:20:00 +0530

    time question

--- 03js/javaScriptAP/javascript2/index.js
+++ 03js/javaScriptAP/javascript2/index.js
@@ -113,7 +113,17 @@
              
 // IF-ELSE STATEMENTS
 
+ let time = 14;
 
+if (time < 12) {
+    console.log("Morning");
+}
+else if (time < 17) {
+    console.log("Afternoon");
+}
+else {
+    console.log("Evening");
+}  
 
 
 
