b10f3f0b9a18ce1a2ba810f53cde378e81c1f170
hyannu <annurao9560@gmail.com>
2026-06-15 16:18:34 +0530

    new folder create loos in js

--- 03js/javaScriptAP/practical question1/question cl2 q/index.js
+++ 03js/javaScriptAP/practical question1/question cl2 q/index.js
@@ -66,21 +66,29 @@
 // console.log("according to your scores, your grade was :", Grade);
 
 
-let score = 85;
-let  grade;
-if(score >= 90) {
-    grade ="A";
-}
-else if(score >= 80) {
-grade= "B";
-}
-else if(score >= 70) {
-    grade ="C";
-}
-else if(score >= 60) {
-    grade ="D";
-}
-else{
-    grade ="fail"
-}
-console.log(grade);
+// let score = 85;
+// let  grade;
+// if(score >= 90) {
+//     grade ="A";
+// }
+// else if(score >= 80) {
+// grade= "B";
+// }
+// else if(score >= 70) {
+//     grade ="C";
+// }
+// else if(score >= 60) {
+//     grade ="D";
+// }
+// else{
+//     grade ="fail"
+// }
+// console.log(grade);
+
+
+
+
+
+
+
+
