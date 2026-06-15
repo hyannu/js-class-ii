0eca4a478c04f354e5d029e5e5ac29c62620624c
hyannu <annurao9560@gmail.com>
2026-06-15 12:30:35 +0530

    exam score question

--- 03js/javaScriptAP/practical question1/question cl2 q/index.js
+++ 03js/javaScriptAP/practical question1/question cl2 q/index.js
@@ -1,3 +1,15 @@
 // QUESTION NUMBER 1
 // Get user to input a number using prompt("Enter a number:").
-// Check if the number is a multiple of 5 or Notification. 
+// Check if the number is a multiple of 5 or Notification.
+
+// alert("hello!");//one time pop
+
+
+// let num = 98763 ;
+// if (num % 5 === 0) {
+//     console.log(num,"multiple of 5");
+// }
+//     else{
+//    console.log(num,"number of not a multiple of 5");
+//     }
+
