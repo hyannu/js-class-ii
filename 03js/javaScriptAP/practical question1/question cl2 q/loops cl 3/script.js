0782140e242209f62ec7e51d4d61c50ce5840a51
hyannu <annurao9560@gmail.com>
2026-06-15 17:06:30 +0530

    calculate sum number in  loops

---
+++ 03js/javaScriptAP/practical question1/question cl2 q/loops cl 3/script.js
@@ -0,0 +1,24 @@
+//loops
+// // print 1 to 10 number
+// for (let i =1;  i <= 10; i++){
+//     console.log(i)
+// }
+
+// // 2 table print
+// for(let i = 2; i <= 20; i++){
+//     console.log(i++)
+// }
+// print 2to20
+// for(let i = 2; i <= 20; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+// calculate sum of 1 to 100 
+let sum = 0;
+let n = 100;
+for(let i = 1; i<=100;i++){
+    
+}
