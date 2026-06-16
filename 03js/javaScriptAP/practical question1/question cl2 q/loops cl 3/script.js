73623c49cc31dd6d0cea286132398ee5b10436f8
hyannu <annurao9560@gmail.com>
2026-06-16 18:22:48 +0530

    for-of loop

--- 03js/javaScriptAP/practical question1/question cl2 q/loops cl 3/script.js
+++ 03js/javaScriptAP/practical question1/question cl2 q/loops cl 3/script.js
@@ -28,9 +28,47 @@
 
 
 // while loops
+// let i = 1;
+// while(i <= 5){
+//     console.log("rlarchu");
+//     i++;
+// }
+
 
 
 // do while loop
+// let i = 1;
+
+// do {
+//     console.log(i);
+// i++;
+// } while (i <= 5);//condition  
+
+
+// let i =10;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i <= 5);//condition false
+
+
+// for of loop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
