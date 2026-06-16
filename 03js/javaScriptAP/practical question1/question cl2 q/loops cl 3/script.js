a1727a0f81e8b357850dfbdb8fd3ae78c3862d74
hyannu <annurao9560@gmail.com>
2026-06-16 15:08:37 +0530

    infinite loop

--- 03js/javaScriptAP/practical question1/question cl2 q/loops cl 3/script.js
+++ 03js/javaScriptAP/practical question1/question cl2 q/loops cl 3/script.js
@@ -19,6 +19,8 @@
 // calculate sum of 1 to 100 
 let sum = 0;
 let n = 100;
-for(let i = 1; i<=100;i++){
-    
+for(let i = 1; i<=n; i++) {
+    sum = sum + i;
 }
+console.log("sum = ", sum);
+console.log("loop has ended");
