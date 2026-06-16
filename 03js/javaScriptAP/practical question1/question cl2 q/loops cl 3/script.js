5865ab8ae56c4cc970c0f25d6d37edf2ee6ba835
hyannu <annurao9560@gmail.com>
2026-06-16 19:54:22 +0530

    for-in loop

--- 03js/javaScriptAP/practical question1/question cl2 q/loops cl 3/script.js
+++ 03js/javaScriptAP/practical question1/question cl2 q/loops cl 3/script.js
@@ -55,6 +55,14 @@
 
 // for of loop
 
+// let str ="javascript";
+// let size =0;
+// for(let i of str){
+//     console.log("i= ", i);
+//     size++;
+// }
+// console.log("string size = ", size);//10
+
 
 
 
