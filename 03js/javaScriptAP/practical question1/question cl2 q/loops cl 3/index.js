7b5c18871eb20558ae983a031792f32ab223fa86
hyannu <annurao9560@gmail.com>
2026-06-16 21:09:02 +0530

    even number print

---
+++ 03js/javaScriptAP/practical question1/question cl2 q/loops cl 3/index.js
@@ -0,0 +1,20 @@
+// Print all even numbers from 0 to 100.
+
+
+// for(let num = 0; num<=100; num++){
+//     console.log("num = ", num);
+// }
+
+// EVEN NUMBER
+// for(let num = 0; num <= 50; num++){
+//     if(num%2 === 0){//even number
+//         console.log("num = ", num);
+//     }
+// }
+
+//ODD NUMBER
+for(let num = 0; num <= 50; num++){
+    if(num %2 !== 0){
+        console.log("num= ", num);
+    }
+}
