2ecb002071368bd13b2614b24641ee43188c7c33
hyannu <annurao9560@gmail.com>
2026-06-26 23:12:40 +0530

    array length define

--- 03js/javaScriptAP/practical question1/question cl2 q/loops cl 3/arrays/script.js
+++ 03js/javaScriptAP/practical question1/question cl2 q/loops cl 3/arrays/script.js
@@ -1,4 +1,8 @@
 //ARRAYS
-let marks_student1 =  97;
-let marks_student2 =
+// let marks_student1 =  97;
+// let marks_student2 =
+
+let heroes = ["ironman", "hulk", "thor","batman"];
+let marks = [96,75,48,83,66];
+let info=["rahul",86,"Delhi"];
      
