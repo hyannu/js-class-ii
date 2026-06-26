f0311392dffe5d8b95873f039c7d0e999369ffde
hyannu <annurao9560@gmail.com>
2026-06-26 23:52:06 +0530

    array indices

--- 03js/javaScriptAP/practical question1/question cl2 q/loops cl 3/arrays/script.js
+++ 03js/javaScriptAP/practical question1/question cl2 q/loops cl 3/arrays/script.js
@@ -2,7 +2,16 @@
 // let marks_student1 =  97;
 // let marks_student2 =
 
-let heroes = ["ironman", "hulk", "thor","batman"];
-let marks = [96,75,48,83,66];
-let info=["rahul",86,"Delhi"];
+/*let heroes = ["ironman", "hulk", "thor","batman",""];
+console.log(heroes);
+console.log(heroes.length);*/
+
+
+
+/*let marks = [97,82, 75, 64, 36];
+console.log(marks);
+console.log(marks.length);*/
+
+
+
      
