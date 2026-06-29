6eb3fd05e8ae29d4a19ba29421e1c86c3340f986
hyannu <annurao9560@gmail.com>
2026-06-29 14:56:43 +0530

    feat: add filter method example to find odd numbers

--- 03js/javaScriptAP/practical question1/question cl2 q/loops cl 3/arrays/functin & methods/script.js
+++ 03js/javaScriptAP/practical question1/question cl2 q/loops cl 3/arrays/functin & methods/script.js
@@ -216,12 +216,12 @@ let calcSquare=(num)=> {
 
 //filter method
 //even number findout
-let arr=[1,2,3,4,5,6,7];
+/*let arr=[1,2,3,4,5,6,7];
 
 let evenArr = arr.filter((val)=> {
     return val % 2===0;
 });
-console.log(evenArr);
+console.log(evenArr);*/
 
 
 
